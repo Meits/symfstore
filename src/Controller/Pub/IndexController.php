2437d9c906e4b8cbafb0876e138facb2658179c3
Viktor <meitslin@gmail.com>
2021-11-20 17:16:40 +0200

    Init

---
+++ src/Controller/Pub/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Pub;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('public/index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
