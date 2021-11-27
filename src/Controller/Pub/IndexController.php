72f9a949cc845b8b13870368055bea0093f0a11b
Viktor <meitslin@gmail.com>
2021-11-27 15:24:47 +0200

    Add entity

--- src/Controller/Pub/IndexController.php
+++ src/Controller/Pub/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Pub;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,32 @@ class IndexController extends AbstractController
 
     public function index(): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+        dd($productList);
+
         return $this->render('public/index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
+    }
+
+
+    /**
+     * @Route("/product-add", methods="GET", name="product-add")
+     */
+    public function productAdd()
+    {
+
+        $product = new Product();
+        $product->setTitle("Product");
+        $product->setDscription("text");
+        $product->setPrice(10);
+        $product->setQuantity(10);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
